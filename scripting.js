75fc1cf4fa50c0dea62c0de3f967fa560ddb38df
Clayton Batiquin <cbatiquin.dev@gmail.com>
2022-02-27 19:18:21 -0600

    Created Script
    started a script and linked to index page

---
+++ scripting.js
@@ -0,0 +1,11 @@
+
+function introductions () {
+    const name = prompt(`State your name.`);
+    const why = prompt(`State your reason of visit.`);
+    const message = `Hi ${name}, glad you stopped by.`;
+    alert(message);
+}
+
+introductions();
+
+
